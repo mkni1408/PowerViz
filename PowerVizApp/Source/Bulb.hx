4d081565c36736ad76bb0db472dffb9231d9ca53
mkni1408 <floop@h385.eduroam-2a.wlan.aalborguniversitet.dk>
2013-10-04 09:56:33 +0200

    screensaver added

---
+++ PowerVizApp/Source/Bulb.hx
@@ -0,0 +1,120 @@
+package;
+
+/*
+
+Bulb object, that defines a bulb
+
+*/
+
+import flash.display.Bitmap;
+import flash.display.Sprite;
+import flash.display.DisplayObject;
+import flash.events.MouseEvent;
+import openfl.Assets;
+
+class Bulb extends Sprite
+{
+
+	private var displayObjects:Array<DisplayObject>;
+
+	private var mBulbOn : Sprite;
+	private var mBulbOff : Sprite;
+
+	private var mOn : Bool;
+
+	public function new()
+	{
+
+		super ();
+		
+
+		displayObjects = new Array<DisplayObject>();
+
+		//Container = new DisplayObjectContainer ();
+		var bitMapBulboff = new Bitmap (Assets.getBitmapData ("assets/bulboff.png"));
+		var bitMapBulbon = new Bitmap (Assets.getBitmapData ("assets/bulbon.png"));
+
+		mBulbOn = new Sprite();
+		mBulbOff = new Sprite();
+
+		
+
+		mBulbOn.addChild (bitMapBulbon);
+
+		mBulbOff.addChild (bitMapBulboff);
+
+		//sets the size of the bulbs
+		mBulbOn.width /= 3;
+		mBulbOn.height /= 3;
+		mBulbOff.width /= 3;
+		mBulbOff.height /= 3;
+
+		displayObjects.push(mBulbOn);
+
+		displayObjects.push(mBulbOff);
+
+		
+		bulb_drawline();
+
+		this.addChild(mBulbOn);
+		this.addChild(mBulbOff);
+
+		mOn = false;
+		
+
+
+		//this.addEventListener (MouseEvent.MOUSE_DOWN, bulb_onMouseDown);
+		
+	}
+
+	private function bulb_drawline()
+	{
+
+		graphics.lineStyle(4,0x000000);
+
+		graphics.moveTo(mBulbOn.width/2,0);
+		graphics.lineTo(mBulbOn.width/2,-500);
+	}
+
+
+
+	//Testfunction: when we click the bulb it should either turn on or off
+	private function bulb_onMouseDown (event:MouseEvent):Void {
+
+		mOn = !mOn;
+		trace(mOn);
+		if(mOn){
+ 			
+ 			mBulbOn.visible = true;
+ 			mBulbOff.visible = false;
+
+
+		}
+		else
+		{
+			mBulbOn.visible = false;
+ 			mBulbOff.visible = true;
+		}
+		
+
+		trace("Mouseeven has been registered!!");
+
+	}
+	//the bulb should turn on
+	public function bulb_changeStateToOn ():Void{
+		
+ 			
+ 			mBulbOn.visible = true;
+ 			mBulbOff.visible = false;
+
+
+	}
+	//the bulb should turn off
+	public function bulb_changeStateToOff ():Void{
+		
+			mBulbOn.visible = false;
+ 			mBulbOff.visible = true;
+		
+
+	}
+}
--- PowerVizApp/Source/ScreenSaver.hx
+++ PowerVizApp/Source/ScreenSaver.hx
@@ -1,57 +1,229 @@
-import flash.display.Bitmap;
-import flash.display.BitmapData;
+package;
+
+
 import flash.display.Sprite;
-import openfl.Assets;
-
 import flash.events.MouseEvent;
-
-/*
-A screensaver:
-Usage: we create a screemsaver that lies on top of the swipemill
-*/
+import flash.Lib;
+import Bulb;
+import Math;
 
 
-class ScreenSaver extends Sprite
-{
+class ScreenSaver extends Sprite {
+	
+	private var firstBulb:Bulb;
+	private var secondBulb:Bulb;
+	private var thirdBulb:Bulb;
+	private var fourthBulb:Bulb;
+	private var fifthBulb:Bulb;
+	private var sixthBulb:Bulb;
+	private var seventhBulb:Bulb;
+	private var mIsTransparant: Bool;
+	private var mHasRecievedTouchEvent: Bool;
 
-	public function new() {
+
+	public function new(){
+
 		super();
 
-		var bitmap = new Bitmap (Assets.getBitmapData ("assets/bulb.png"));
-		bitmap.width = HWUtils.screenWidth;
-		bitmap.height = HWUtils.screenHeight;
-		this.addChild (bitmap);
-		//registerEvents();
-
-	}
-	//supposed to show the Sprite from dimmed state
-	public function brighten(){
-
-	}
+		mIsTransparant = true;
+		mHasRecievedTouchEvent = false;
 
 
-	//supposed to dim the Sprite from brightened state
-	public function dim(){
+		firstBulb = new Bulb();
+		secondBulb = new Bulb();
+		thirdBulb = new Bulb();
+		fourthBulb = new Bulb();
+		fifthBulb = new Bulb();
+		sixthBulb = new Bulb();
+		seventhBulb = new Bulb();
+
+		//add bulbobjects to main sprite
+		addChild(firstBulb);
+		addChild(secondBulb);
+		addChild(thirdBulb);
+		addChild(fourthBulb);
+
+		addChild(fifthBulb);
+		addChild(sixthBulb);
+		addChild(seventhBulb);
+
+
+		
+
+
+		
+		//place the bulbs
+		placeBulbs();
+
+		setBulbAction(50);
+
+		this.mouseEnabled = false;
+		this.addEventListener (MouseEvent.MOUSE_DOWN, screen_onMouseDown);
 
 	}
 	
-	/*
-	private function registerEvents() {
-	
-	
-		#if desktop
-			this.addEventListener(MouseEvent.MOUSE_DOWN, someMouseEvent);
-			this.addEventListener(MouseEvent.MOUSE_UP, someMouseEvent);
-			this.addEventListener(MouseEvent.MOUSE_MOVE, someMouseEvent);
-			this.addEventListener(MouseEvent.MOUSE_OUT, someMouseEvent);
-		#end
-	
+//Testfunction: when we click the bulb it should either turn on or off
+	private function screen_onMouseDown (event:MouseEvent):Void {
+
+		
+		this.alpha = 0;
+		mHasRecievedTouchEvent = false;
+		trace("Mouseeven has been registered!!");
+
 	}
-	
-	private function someMouseEvent(event:MouseEvent) {
-		trace("event: " + event);
-	}
-	*/
+	//starts timers that 1, updates bulbs and 2, checks if screen has been tuched.
+	public function setBulbAction(watt:Float):Void {
+			var yourbulbTimer:haxe.Timer = new haxe.Timer(3000);
+
+			yourbulbTimer.run = function():Void{
+   			trace("BulbTimer running!!");
+   				
+
+   				var f  = Math.random();
+
+   				trace(f);
+
+   				changeBulbStates(f);
+			};
 
 
+			var yourScreensaverTimerTimer:haxe.Timer = new haxe.Timer(10000);
+
+			yourScreensaverTimerTimer.run = function():Void{
+   			trace("Screen saver Timer running!!");
+   				
+   				trace(mIsTransparant);
+   				trace(mHasRecievedTouchEvent);
+   				setScreenSaver();
+   				
+   				
+
+
+			};
+
+	}
+	//change the bulbstates man
+	public function changeBulbStates(f:Float):Void{
+				
+				if(f < 0.2){
+
+   					firstBulb.bulb_changeStateToOn();
+   					secondBulb.bulb_changeStateToOff();
+					thirdBulb.bulb_changeStateToOff();
+					fourthBulb.bulb_changeStateToOff();
+					fifthBulb.bulb_changeStateToOff();
+
+   					trace("first bulb is on");
+
+   				}	
+   				else if(f > 0.2 && f < 0.4){
+   					firstBulb.bulb_changeStateToOn();
+   					secondBulb.bulb_changeStateToOn();
+					thirdBulb.bulb_changeStateToOff();
+					fourthBulb.bulb_changeStateToOff();
+					fifthBulb.bulb_changeStateToOff();
+
+   					trace("first and second bulb is on");
+   				}
+   				else if(f > 0.4 && f < 0.6){
+   					firstBulb.bulb_changeStateToOn();
+   					secondBulb.bulb_changeStateToOn();
+					thirdBulb.bulb_changeStateToOn();
+					fourthBulb.bulb_changeStateToOff();
+					fifthBulb.bulb_changeStateToOff();
+   					trace("first, second and third bulb is on");
+   				}
+   				else if(f > 0.6 && f < 0.8){
+   					firstBulb.bulb_changeStateToOn();
+   					secondBulb.bulb_changeStateToOn();
+					thirdBulb.bulb_changeStateToOn();
+					fourthBulb.bulb_changeStateToOn();
+					fifthBulb.bulb_changeStateToOff();
+   					trace("first, second, third and fourth bulb is on");
+   				}
+   				else if(f > 0.8 && f <= 1){
+   					firstBulb.bulb_changeStateToOn();
+   					secondBulb.bulb_changeStateToOn();
+					thirdBulb.bulb_changeStateToOn();
+					fourthBulb.bulb_changeStateToOn();
+					fifthBulb.bulb_changeStateToOn();
+   					trace("first, second, third, fourth and fifth bulb is on");
+   				}
+
+
+
+	}
+	//set screensaverstate. If the screensaver is transparent and we have not recieved any Touchevents, 
+	//we make it visible. Else we make it invisible
+	public function setScreenSaver():Void{
+
+				if(!mIsTransparant && !mHasRecievedTouchEvent)
+   				{
+   					this.alpha = 1;
+   					mIsTransparant = true;
+   					mHasRecievedTouchEvent = false;
+   				}
+   				else if(mIsTransparant && !mHasRecievedTouchEvent)
+   				{
+					this.alpha = 1;
+					mIsTransparant = false;
+					mHasRecievedTouchEvent = false;
+
+
+   				}
+   				else
+   				{
+
+
+   				}
+
+	}
+	//Place the bulbs
+	public function placeBulbs()
+	{
+
+
+		var positionHorisontal = (Lib.stage.stageWidth/10);
+		var positionVertical = 0+40;
+		var positionVerticaloffset = 65;
+
+		//position the bulbs
+		firstBulb.x = positionHorisontal;
+		firstBulb.y = positionVertical+45;
+
+
+		secondBulb.x = positionHorisontal*3;
+		secondBulb.y = positionVertical+20;
+
+
+
+		thirdBulb.x = positionHorisontal*5;
+		thirdBulb.y = positionVertical+5;
+
+
+		fourthBulb.x = positionHorisontal*7;
+		fourthBulb.y = positionVertical+25;
+
+
+
+		fifthBulb.x = positionHorisontal+positionVerticaloffset;
+		fifthBulb.y = (Lib.stage.stageHeight/2)+30;
+
+
+
+		sixthBulb.x = (positionHorisontal*3)+positionVerticaloffset;
+		sixthBulb.y = (Lib.stage.stageHeight/2);
+
+
+
+
+		seventhBulb.x = (positionHorisontal*5)+positionVerticaloffset;
+		seventhBulb.y = (Lib.stage.stageHeight/2);
+	}
+
+
+
+
+
+	
 }
