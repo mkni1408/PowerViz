bdfc6e13bfc3c06de4625bae3ebe7862e89b3bfc
Dlund <dklund89@gmail.com>
2013-10-08 21:41:35 +0200

    Decreased the space between bars

--- PowerVizApp/Source/BarGraph.hx
+++ PowerVizApp/Source/BarGraph.hx
@@ -11,7 +11,7 @@ class BarGraph extends Sprite {
 	
 	public function drawBar(colors:Array<Int>, height:Array<Float>) {
 	
-		//This is a hack:
+		//This is a hack, which draws a transparent rectangle of 1x1 pixel
 		this.graphics.beginFill(0x000000,0.0);
 		this.graphics.drawRect(0,0, 1,1);
 		this.graphics.endFill();
@@ -20,13 +20,16 @@ class BarGraph extends Sprite {
 		var barWidth:Float = Lib.stage.stageWidth/colors.length;
 		
 		//This variable is used as the starting point on the x-axis
-		var xCoord:Float =(0.25*barWidth);
+		var xCoord:Float =(0.10*barWidth);
 		
 		var i:Int = 0;
 		for(c in colors) {
+			// For every c in colors a rectangle will be drawn and colored.
 			this.graphics.beginFill(colors[i]);
-			this.graphics.drawRect(xCoord, 0, barWidth*0.75, -height[i]);			
+			this.graphics.drawRect(xCoord, 0, barWidth*0.90, -height[i]);			
 			this.graphics.endFill();
+			
+			//Increment with barWidth and not barWidth*0.90 (as in drawRect()) is to make a space between each bar.
 			xCoord += barWidth; 
 			
 			i += 1; //Increment by one
