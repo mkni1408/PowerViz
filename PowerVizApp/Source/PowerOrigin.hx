6215fca1b6a630ae3d421197612eda85b2b81c55
Dlund <dklund89@gmail.com>
2013-10-25 14:34:27 +0200

    PowerOrigin made pretty :)

--- PowerVizApp/Source/PowerOrigin.hx
+++ PowerVizApp/Source/PowerOrigin.hx
@@ -65,37 +65,29 @@ class PowerOrigin extends Sprite{
 		origin.width = Lib.stage.stageWidth/2;
 		origin.height = Lib.stage.stageHeight/2;
 
-			var centerBitmapX = origin.width/2;
-			var centerBitmapY = origin.height/2;
+		var centerBitmapX = origin.width/2;
+		var centerBitmapY = origin.height/2;
 
-			var centerSpriteX = (Lib.stage.stageWidth / 4)*3;
-			var centerSpriteY = (Lib.stage.stageHeight / 4);
+		var centerSpriteX = (Lib.stage.stageWidth / 4)*3;
+		var centerSpriteY = (Lib.stage.stageHeight / 4);
 
-			origin.x = centerSpriteX - centerBitmapX;
-			origin.y = centerSpriteY - centerBitmapY;
+		origin.x = centerSpriteX - centerBitmapX;
+		origin.y = centerSpriteY - centerBitmapY;
 	}
 
 	public function changePowerOrigin():Void{
-
 		/*
 		var powerOrigin = DataInterface.instance.method();
 		
 		//pull powerorigin and change which sprite is visible
-		if(powerOrigin == "wind")
-		{
-		changeOrigin(true,false,false,false);
-		}
-		else if(powerOrigin == "coal")
-		{
-		changeOrigin(false,true,false,false);
-		}
-		else if(powerOrigin == "solar")
-		{
-		changeOrigin(false,false,true,false);
-		}
-		else if(powerOrigin == "nuclear")
-		{
-		changeOrigin(false,false,false,true);
+		if(powerOrigin == "wind") {
+			changeOrigin(true,false,false,false);
+		} else if(powerOrigin == "coal") {
+			changeOrigin(false,true,false,false);
+		} else if(powerOrigin == "solar") {
+			changeOrigin(false,false,true,false);
+		} else if(powerOrigin == "nuclear") {
+			changeOrigin(false,false,false,true);
 		}
 		*/
 		
@@ -103,52 +95,39 @@ class PowerOrigin extends Sprite{
 		var changenumber = Std.random(4);
 
 		//pull powerorigin and change which sprite is visible
-		if(changenumber == 0)
-		{
-		changeOrigin(true,false,false,false);
-		}
-		else if(changenumber == 1)
-		{
-		changeOrigin(false,true,false,false);
-		}
-		else if(changenumber == 2)
-		{
-		changeOrigin(false,false,true,false);
-		}
-		else if(changenumber == 3)
-		{
-		changeOrigin(false,false,false,true);
+		if(changenumber == 0) {
+			changeOrigin(true,false,false,false);
+		} else if(changenumber == 1) {
+			changeOrigin(false,true,false,false);
+		} else if(changenumber == 2) {
+			changeOrigin(false,false,true,false);
+		} else if(changenumber == 3) {
+			changeOrigin(false,false,false,true);
 		}
 	}
 
 	private function changeOrigin(first:Bool,second:Bool,third:Bool,fourth:Bool):Void{
 
 		if(first){
-		Actuate.tween (mPowerOriginCoal, 2, { alpha: 0 } );
-		Actuate.tween (mPowerOriginSolar, 2, { alpha: 0 } );
-		Actuate.tween (mPowerOriginNuclear, 2, { alpha: 0 } );
-		Actuate.tween (mPowerOriginWind, 2, { alpha: 1 } );
-		}
-		else if(second)
-		{
-		Actuate.tween (mPowerOriginWind, 2, { alpha: 0 } );
-		Actuate.tween (mPowerOriginSolar, 2, { alpha: 0 } );
-		Actuate.tween (mPowerOriginNuclear, 2, { alpha: 0 } );
-		Actuate.tween (mPowerOriginCoal, 2, { alpha: 1 } );
-		}
-		else if(third) 
-		{
-		Actuate.tween (mPowerOriginWind, 2, { alpha: 0 } );
-		Actuate.tween (mPowerOriginCoal, 2, { alpha: 0 } );
-		Actuate.tween (mPowerOriginNuclear, 2, { alpha: 0 } );
-		Actuate.tween (mPowerOriginSolar, 2, { alpha: 1 } );
-		}
-		else
-		{
-		Actuate.tween (mPowerOriginWind, 2, { alpha: 0 } );
-		Actuate.tween (mPowerOriginCoal, 2, { alpha: 0 } );
-		Actuate.tween (mPowerOriginSolar, 2, { alpha: 0 } );
-		Actuate.tween (mPowerOriginNuclear, 2, { alpha: 1 } );
+			Actuate.tween (mPowerOriginCoal, 2, { alpha: 0 } );
+			Actuate.tween (mPowerOriginSolar, 2, { alpha: 0 } );
+			Actuate.tween (mPowerOriginNuclear, 2, { alpha: 0 } );
+			Actuate.tween (mPowerOriginWind, 2, { alpha: 1 } );
+		} else if(second) {
+			Actuate.tween (mPowerOriginWind, 2, { alpha: 0 } );
+			Actuate.tween (mPowerOriginSolar, 2, { alpha: 0 } );
+			Actuate.tween (mPowerOriginNuclear, 2, { alpha: 0 } );
+			Actuate.tween (mPowerOriginCoal, 2, { alpha: 1 } );
+		} else if(third) {
+			Actuate.tween (mPowerOriginWind, 2, { alpha: 0 } );
+			Actuate.tween (mPowerOriginCoal, 2, { alpha: 0 } );
+			Actuate.tween (mPowerOriginNuclear, 2, { alpha: 0 } );
+			Actuate.tween (mPowerOriginSolar, 2, { alpha: 1 } );
+		} else {
+			Actuate.tween (mPowerOriginWind, 2, { alpha: 0 } );
+			Actuate.tween (mPowerOriginCoal, 2, { alpha: 0 } );
+			Actuate.tween (mPowerOriginSolar, 2, { alpha: 0 } );
+			Actuate.tween (mPowerOriginNuclear, 2, { alpha: 1 } );
 		}
 	}
 	
